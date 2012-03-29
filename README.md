697035322c1f0d219784d1f44c60b201d8abf6b1
joergschiller <joergschiller@googlemail.com>
2012-03-29 14:45:17 +0200

    First version and initial commit.

---
+++ README.md
@@ -0,0 +1,31 @@
+# USB Missile Launcher
+
+Controls the Syntek USB Missile Launcher (USB VendorID 2123 and ProductID 1010).
+
+There are a few other projects for using launchers in Linux, the one that I found was written in Python and heavily inspired me to create this Ruby approach: https://github.com/nmilford/stormLauncher
+
+You need to be root or add the following rule to udev (e.g. /etc/udev/rules.d/80-missile-launcher.rules)
+
+    SUBSYSTEM=="usb", ATTR{idVendor}=="2123", ATTR{idProduct}=="1010", GROUP="games"
+
+Make sure your restart udev, unplug/plug the launcher to your pc.
+
+    sudo service udev restart
+
+Please put the current user into the games group, and log out and login afterwards.
+
+    sudo gpasswd -a `whoami` games
+
+## Getting Started:
+
+    git clone git@github.com:joergschiller/launcher.git
+    cd launcher
+    bundle
+
+## Usage:
+
+    irb -r launcher -I .
+    1.9.3-p0 :001 > launcher = Launcher.new
+    1.9.3-p0 :001 > launcher.up # steps turret up
+    1.9.3-p0 :001 > launcher.left 500 # moves turret left for 500ms
+    1.9.3-p0 :001 > launcher.fire
---
+++ launcher.rb
@@ -0,0 +1,51 @@
+require "rubygems"
+require "bundler/setup"
+require "usb"
+
+# Usage example
+#
+#     launcher = Launcher.new
+#     launcher.up # steps turret up, available commands are: up, down, left, right, stop, fire
+#     launcher.up 500 # moves turret up for 500ms
+
+class Launcher
+
+  COMMANDS = { :up => "\x02\x02\x00\x00\x00\x00\x00\x00",
+               :down => "\x02\x01\x00\x00\x00\x00\x00\x00",
+               :left => "\x02\x04\x00\x00\x00\x00\x00\x00",
+               :right => "\x02\b\x00\x00\x00\x00\x00\x00",
+               :stop => "\x02 \x00\x00\x00\x00\x00\x00",
+               :fire => "\x02\x10\x00\x00\x00\x00\x00\x00" }
+
+  def initialize
+    @launcher = USB.devices.select { |d| d.idVendor == 0x2123 && d.idProduct == 0x1010 }.first
+
+    @launcher.open do |h|
+      begin
+        h.usb_detach_kernel_driver_np 0, 0
+      rescue Errno::ENODATA => e
+      end
+    end
+  end
+
+  def fire
+    execute :fire
+  end
+
+  def method_missing(command, *args)
+    if COMMANDS.keys.include? command
+      duration = args.first || 25
+
+      execute command
+      sleep duration * 0.001
+      execute :stop
+    end
+  end
+
+  private
+
+    def execute(command)
+      @launcher.open { |h| h.usb_control_msg 0x21, 0x09, 0, 0, COMMANDS[command], 0 }
+    end
+
+end
